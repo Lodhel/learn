953b4e5dc430923eb8bc1493ab7f99c352b669eb
Lodhel <lodhelnew@gmail.com>
2021-06-16 20:59:19 +0300

    урок 1. многопоточное программирование. обедающие философы.

---
+++ 0001/run.py
@@ -0,0 +1,36 @@
+import random
+import threading
+
+philosophs = {1: False, 2: False, 3: False, 4: False, 5: False}
+warehouse = [1, 2, 3, 4, 5]
+
+
+warehouse_lock = threading.Lock()
+
+overflow_condition = threading.Condition()
+
+
+def is_underflow():
+    return len(warehouse) < 2
+
+
+def philosopher(philosoph):
+    while True:
+        warehouse_lock.acquire()
+        if not is_underflow() and not philosophs[philosoph]:
+            x = warehouse.pop(0), warehouse.pop(1)
+            philosophs[philosoph] = True
+            print('Thread is phil {} take a forks is {}'.format(philosoph, x))
+            warehouse_lock.release()
+            threading.Condition()
+        else:
+            philosophs[philosoph] = False
+            warehouse.append(random.randint(1, 5)), warehouse.append(random.randint(1, 5))
+            warehouse_lock.release()
+            print("Underflow. phil {} waiting".format(philosoph))
+            threading.Condition()
+
+
+for philosoph in philosophs:
+    th = threading.Thread(target=philosopher, args=(philosoph, ))
+    th.start()

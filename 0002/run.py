4c7d5affb77a6cebf0712e220f299a80ae4e7989
Lodhel <lodhelnew@gmail.com>
2021-06-21 21:23:44 +0300

    урок 2. асинхронное программирование. продюсер консьюмер. refactor

--- 0002/run.py
+++ 0002/run.py
@@ -1,44 +1,41 @@
 import asyncio
 import random
+import time
 
 
 warehouse = []
 max_elements = 10
 
 
-def is_overflow():
+def is_overflow() -> True:
     return len(warehouse) >= max_elements
 
 
-def is_underflow():
+def is_underflow() -> 1:
     return len(warehouse) == 0
 
 
-async def producer():
+async def producer() -> None:
     x = random.randint(a=1, b=10000)
     print('produced number {}'.format(x))
     if not is_overflow():
         warehouse.append(x)
     else:
+        await consumer()
         print("Overflow. Producer waiting")
-    await asyncio.sleep(1)
 
 
-async def consumer():
+async def consumer() -> None:
     if not is_underflow():
         x = warehouse.pop(0)
         print('consumed number {}'.format(x))
     else:
-        print("Underflow. Consumer waiting")
-    await asyncio.sleep(1)
-
-
-async def main():
-    while True:
         await producer()
-        await consumer()
-        asyncio.sleep(2)
+        print("Underflow. Consumer waiting")
 
 
-ioloop = asyncio.get_event_loop()
-ioloop.run_until_complete(main())
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    while True:
+        loop.run_until_complete(asyncio.gather(asyncio.Task(producer()), asyncio.Task(consumer())))
+        time.sleep(1)
